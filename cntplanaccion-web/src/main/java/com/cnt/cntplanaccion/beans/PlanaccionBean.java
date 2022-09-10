55cd7172f2cf6d5a8255f9f32040d133c5a6e7f5
fvillavicencio <fvillavicencio@SISINQ4PAQTR06.andinatel.int>
2022-09-09 21:37:42 -0500

    consultarTodos, crud para tabla de tipo de informes, combobox formulario de planes

--- cntplanaccion-web/src/main/java/com/cnt/cntplanaccion/beans/PlanaccionBean.java
+++ cntplanaccion-web/src/main/java/com/cnt/cntplanaccion/beans/PlanaccionBean.java
@@ -7,10 +7,12 @@ package com.cnt.cntplanaccion.beans;
 import com.matoosfe.unomina.beans.util.AbstractManagedBean;
 import com.cnt.planaccion.PlanCab;
 import com.cnt.planaccion.controllers.PlanCabFacade;
+import com.cnt.planaccion.controllers.PlanEstFacade;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -25,13 +27,18 @@ public class PlanaccionBean extends AbstractManagedBean implements Serializable{
     private PlanCab plan;
     private PlanCab planSel;
     private List<PlanCab> listaPlanes;
+    private List<SelectItem> listaEstados;
+    private Integer idEst;
     
     @Inject //Luego del constructor
     private PlanCabFacade adminPlan;
+    @Inject
+    private PlanEstFacade adminEstados;
     
     public PlanaccionBean() {
         this.plan = new PlanCab(); //Encerando un registro de la tabla
         this.listaPlanes = new ArrayList<>(); //Encerando la lista
+        this.listaEstados = new ArrayList<>();
     }
 
     public PlanCab getPlan() {
@@ -57,6 +64,24 @@ public class PlanaccionBean extends AbstractManagedBean implements Serializable{
     public void setListaPlanes(List<PlanCab> listaPlanes) {
         this.listaPlanes = listaPlanes;        
     }
+
+    public List<SelectItem> getListaEstados() {
+        return listaEstados;
+    }
+
+    public void setListaEstados(List<SelectItem> listaEstados) {
+        this.listaEstados = listaEstados;
+    }
+
+    public Integer getIdEst() {
+        return idEst;
+    }
+
+    public void setIdEst(Integer idEst) {
+        this.idEst = idEst;
+    }
+    
+    
     
     public void guardar() {
         try {
@@ -76,6 +101,11 @@ public class PlanaccionBean extends AbstractManagedBean implements Serializable{
         }
     }
     
+    private void cargarEstados() {
+        adminEstados.consultarTodos().forEach(est -> this.listaEstados
+                .add(new SelectItem(est.getEstId(), est.getEstDesc())));
+    }
+    
     private void cargarPlanes() {
         this.listaPlanes = adminPlan.consultarTodos();
     }
@@ -88,5 +118,6 @@ public class PlanaccionBean extends AbstractManagedBean implements Serializable{
     @PostConstruct
     public void inicializar() {
         cargarPlanes();
+        cargarEstados();
     }
 }
